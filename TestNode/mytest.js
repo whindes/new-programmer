7fa4cb61b253a9d39797a9fc42018b504f0928ca
ahindes91 <alexhindes@ymail.com>
2017-05-11 00:12:23 -0400

    Update mytest.js
    Live Edit

--- TestNode/mytest.js
+++ TestNode/mytest.js
@@ -1,5 +1,5 @@
 var message="sup fool";
 //alert (message);
-var finalmessage=message+" this is cool";
+var finalmessage=message+" this is really cool";
 console.log(finalmessage);
 //alert (finalmessage);
